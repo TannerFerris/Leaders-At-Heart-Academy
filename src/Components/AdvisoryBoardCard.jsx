a3b94c5020bbf8177609cd5438602c38a0a997b6
Tanner Foster <tannerfoster@ferristexas.gov>
2025-01-29 11:24:18 -0600

    Added Ofelia Grass Bio

--- src/Components/AdvisoryBoardCard.jsx
+++ src/Components/AdvisoryBoardCard.jsx
@@ -1,68 +1,125 @@
 /* eslint-disable react/no-unescaped-entities */
-import './AdvisoryBoardCard.css';
+import "./AdvisoryBoardCard.css";
 function AdvisoryBoard() {
   return (
-        <main className="mainAdvisoryBoard">
-                          <section className="Advisorysection">
-                          <div className="AdvisoryCardcontainer2">
-                    <h1>Chad Newsom, Board President</h1>
-                        </div>
-                <div className="AdvisoryCardcontainer2">
-                    <h1>Ofelia Grass</h1>
-                        </div>
-                </section>
-            <section className="Advisorysection">
-                <div className="AdvisoryCardcontainer">
-                    <h1>Norman “Coach” Heckathorn</h1>
-                    <h2>Coach Heckathorn has dedicated 49 years to the field of education, building an exceptional career as 
-                      both a teacher and an administrator. A lifelong advocate for learning and student success, Norman holds 
-                      a Bachelor’s Degree from East Texas University (1967) and a Master’s Degree from Prairie View A&M University (1981).
-                      Coach’s passion for education is deeply rooted in his family. His wife served as an educational secretary for 26 years, 
-                      and all four of his children have followed in his footsteps as educators.
-                      <br/><br/>
-                      Throughout his career, Coach has gained a wealth of experience across various roles, including teacher, coach, athletic 
-                      director, and administrator. He has worked in school districts ranging from 1A to 5A, giving him a broad perspective on 
-                      the challenges and opportunities within diverse educational settings. Whether leading small teams as an Athletic Director 
-                      with just two staff members or managing larger programs with 24 staff members, Norman has consistently demonstrated 
-                      strong leadership and a commitment to excellence.
-                      In addition to his extensive leadership experience, Coach has a strong background in teaching social studies and history, 
-                      equipping him with an understanding of the academic and social needs of students.
-                      <br/><br/>
-                      Now retired, Coach remains passionate about creating environments where both educators and students can thrive. His 
-                      decades of experience position him as a valuable advocate for innovative educational initiatives, including charter 
-                      schools. Coach’s dedication to fostering student success and supporting educators continues to guide his commitment 
-                      to making a lasting impact in the field of education.
-                      </h2>
-                        </div>
-                        <div className="AdvisoryCardcontainer">
-                    <h1>Ron Smith</h1>
-                    <h2>Ron Smith is a distinguished education professional with a wealth of experience in business management and leadership. 
-                      Serving as the Business Manager for Ferris ISD from 1989 to 2006, and later as Assistant Superintendent for Business 
-                      from 2007 to 2013, Ron consistently demonstrated exceptional expertise in financial management, policy compliance, and 
-                      staff leadership.
-                      <br/><br/>
-                      With a robust background in accounting and business operations, Ron excelled in managing complex tasks under tight 
-                      deadlines, ensuring the seamless and efficient operation of the district's business functions. Prior to his tenure 
-                      at Ferris ISD, he served as a Bookkeeper and Accounting Manager at Terrell ISD from 1974 to 1989. In this role, he 
-                      oversaw critical responsibilities such as bank reconciliations, general ledger maintenance, investments, and daily 
-                      deposits.
-                      <br/><br/>
-                      At Ferris ISD, Ron was instrumental in overseeing budgeting, payroll, insurance, and other essential financial 
-                      operations. He also provided visionary leadership to staff and conducted professional development programs for 
-                      teachers, administrators, and support personnel, leaving a lasting impact on the district. Ron is the founder and 
-                      chairman of the Ellis County Business Users Group, established in 1993. This organization, which he continues to lead, 
-                      brings together CFOs, Business Managers, Accounts Payable staff, Payroll staff, HR staff, Purchasing staff, and other 
-                      Business Office personnel. The group currently includes 11 school districts and two charter schools, fostering 
-                      collaboration and knowledge sharing among its members.
-                      <br/><br/>
-                      A native of Dallas and Terrell, Texas, Ron comes from a close-knit family with two siblings. He is a proud father of 
-                      three children, grandfather to nine grandchildren, and soon-to-be great-grandfather. He also enjoys the company of his 
-                      Persian cat, Winston.
-                      </h2>
-                        </div>
-                </section>
-        </main>
-  )
+    <main className="mainAdvisoryBoard">
+      <section className="Advisorysection">
+        <div className="AdvisoryCardcontainer">
+          <h1>Chad Newsom, Board President</h1>
+        </div>
+        <div className="AdvisoryCardcontainer">
+          <h1>Ofelia Grass</h1>
+          <h2>
+            Ofelia Grass is the Owner and Operations Manager of Steam Solutions,
+            Inc. in Ferris, where she oversees the day-to-day operations of the
+            business, including accounting, human resources, and quality
+            control. With extensive experience in leadership roles, Ofelia has a
+            strong background in organizational management, employee relations,
+            and education.
+            <br />
+            <br />
+            Before founding Steam Solutions, Ofelia served the Ferris community
+            as an educator and administrator. She was the Assistant
+            Superintendent at Ferris ISD from 2004 to 2015, where she led
+            recruitment, selection, and orientation processes, developed and
+            implemented salary and compensation policies, and worked
+            collaboratively with teachers and parents to enhance educational
+            programs. Prior to that, she was the Principal of Ferris
+            Intermediate School, managing faculty and staff and ensuring the
+            overall educational success of the school.
+            <br />
+            <br />
+            Ofelia holds a Bachelor of Science degree from Texas Woman’s
+            University and a Master of Education from Texas A&M
+            University-Commerce.
+            <br />
+            <br />
+            She grew up in Ferris, Texas as one of 12 children. She and her
+            husband Robert currently have 2 children and 7 grandchildren.
+          </h2>
+        </div>
+      </section>
+      <section className="Advisorysection">
+        <div className="AdvisoryCardcontainer">
+          <h1>Norman “Coach” Heckathorn</h1>
+          <h2>
+            Coach Heckathorn has dedicated 49 years to the field of education,
+            building an exceptional career as both a teacher and an
+            administrator. A lifelong advocate for learning and student success,
+            Norman holds a Bachelor’s Degree from East Texas University (1967)
+            and a Master’s Degree from Prairie View A&M University (1981).
+            Coach’s passion for education is deeply rooted in his family. His
+            wife served as an educational secretary for 26 years, and all four
+            of his children have followed in his footsteps as educators.
+            <br />
+            <br />
+            Throughout his career, Coach has gained a wealth of experience
+            across various roles, including teacher, coach, athletic director,
+            and administrator. He has worked in school districts ranging from 1A
+            to 5A, giving him a broad perspective on the challenges and
+            opportunities within diverse educational settings. Whether leading
+            small teams as an Athletic Director with just two staff members or
+            managing larger programs with 24 staff members, Norman has
+            consistently demonstrated strong leadership and a commitment to
+            excellence. In addition to his extensive leadership experience,
+            Coach has a strong background in teaching social studies and
+            history, equipping him with an understanding of the academic and
+            social needs of students.
+            <br />
+            <br />
+            Now retired, Coach remains passionate about creating environments
+            where both educators and students can thrive. His decades of
+            experience position him as a valuable advocate for innovative
+            educational initiatives, including charter schools. Coach’s
+            dedication to fostering student success and supporting educators
+            continues to guide his commitment to making a lasting impact in the
+            field of education.
+          </h2>
+        </div>
+        <div className="AdvisoryCardcontainer">
+          <h1>Ron Smith</h1>
+          <h2>
+            Ron Smith is a distinguished education professional with a wealth of
+            experience in business management and leadership. Serving as the
+            Business Manager for Ferris ISD from 1989 to 2006, and later as
+            Assistant Superintendent for Business from 2007 to 2013, Ron
+            consistently demonstrated exceptional expertise in financial
+            management, policy compliance, and staff leadership.
+            <br />
+            <br />
+            With a robust background in accounting and business operations, Ron
+            excelled in managing complex tasks under tight deadlines, ensuring
+            the seamless and efficient operation of the district's business
+            functions. Prior to his tenure at Ferris ISD, he served as a
+            Bookkeeper and Accounting Manager at Terrell ISD from 1974 to 1989.
+            In this role, he oversaw critical responsibilities such as bank
+            reconciliations, general ledger maintenance, investments, and daily
+            deposits.
+            <br />
+            <br />
+            At Ferris ISD, Ron was instrumental in overseeing budgeting,
+            payroll, insurance, and other essential financial operations. He
+            also provided visionary leadership to staff and conducted
+            professional development programs for teachers, administrators, and
+            support personnel, leaving a lasting impact on the district. Ron is
+            the founder and chairman of the Ellis County Business Users Group,
+            established in 1993. This organization, which he continues to lead,
+            brings together CFOs, Business Managers, Accounts Payable staff,
+            Payroll staff, HR staff, Purchasing staff, and other Business Office
+            personnel. The group currently includes 11 school districts and two
+            charter schools, fostering collaboration and knowledge sharing among
+            its members.
+            <br />
+            <br />A native of Dallas and Terrell, Texas, Ron comes from a
+            close-knit family with two siblings. He is a proud father of three
+            children, grandfather to nine grandchildren, and soon-to-be
+            great-grandfather. He also enjoys the company of his Persian cat,
+            Winston.
+          </h2>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default AdvisoryBoard;
