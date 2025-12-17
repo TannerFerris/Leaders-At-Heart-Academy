2f7bb274c046ccc5273f714f23566929696ebc22
TannerFerris <tannerfoster@ferristexas.gov>
2025-12-17 08:32:55 -0600

    Upgrading dependencies and react to 19.2.3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     watch: {
       usePolling: true
